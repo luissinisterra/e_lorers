fde2680873c79210d2789addf86bcf3f4646e4d3
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2026-03-06 12:47:12 -0500

    basic structure implemented

---
+++ lorers_back/src/entities/User.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+  dialect: "postgres", 
+  host: "localhost",
+  username: "your_username",
+  password: "your_password",
+  database: "your_database",
+});
+
+class User extends Model {
+  public id!: number;
+  public name!: string;
+  public username!: string;
+  public password!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+  },
+);
