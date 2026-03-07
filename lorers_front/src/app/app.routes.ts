ef8c1021fce6b6c6cc78fc78932bcaed5f8ccaab
alej0nt <alej0nt666@gmail.com>
2026-03-07 03:35:51 -0500

    Hecho crear eventos

--- lorers_front/src/app/app.routes.ts
+++ lorers_front/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CreateEventComponent } from './shared/components/create-event/create-event';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CreateEventComponent },
+];
