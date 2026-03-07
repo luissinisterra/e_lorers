5f8174214d4c86ee8dcc3634d4bcee81aa072f51
alej0nt <alej0nt666@gmail.com>
2026-03-07 14:13:19 -0500

    Cambio la url de llamada

--- lorers_front/src/app/services/auth.service.ts
+++ lorers_front/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://e-lorers-aa66.onrender.com';
 const TOKEN_KEY = 'lorers_token';
 
 @Injectable({ providedIn: 'root' })
