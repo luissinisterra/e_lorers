03521ad2a17eaeb155885f1e7c50d4d4cd51cd0d
alej0nt <alej0nt666@gmail.com>
2026-03-07 03:36:35 -0500

    Solucionado error

--- lorers_front/src/app/app.routes.ts
+++ lorers_front/src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { CreateEventComponent } from './shared/components/create-event/create-event';
 
-export const routes: Routes = [
-  { path: '', component: CreateEventComponent },
-];
+export const routes: Routes = [];
